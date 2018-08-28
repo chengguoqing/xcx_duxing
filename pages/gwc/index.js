35368382089cec6bf949090587389c2c869c600a
duxing <515235416@qq.com>
2018-08-28 13:36:53 +0800

    添加发布需求页面

--- pages/gwc/index.js
+++ pages/gwc/index.js
@@ -5,62 +5,76 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
+  },
+  fabuxyq() {
+    wx.showModal({
+      title: '提示',
+      content: '发布成功！可否添加个微信？',
+      success: function(res) {
+        if (res.confirm) {
+          wx.previewImage({
+            urls: ["http://duxinggj-2018-1251133427.cosgz.myqcloud.com/erweima.jpg"] // 需要预览的图片http链接列表
+          })
+         
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
